53bac6763b3d3948178e61a72b2d89650c12ef0b
Kris Tokarz <kj.tokarz@gmail.com>
2026-01-10 07:55:40 -0500

    Initial setup

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/tokarzkj/rref-explorer/m/v2
+
+go 1.25.5
+
+require (
+	github.com/KarpelesLab/weak v0.1.1 // indirect
+	github.com/diamondburned/gotk4/pkg v0.3.1 // indirect
+	go4.org/unsafe/assume-no-moving-gc v0.0.0-20231121144256-b99613f794b6 // indirect
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
+)
---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"os"
+
+	"github.com/diamondburned/gotk4/pkg/gio/v2"
+	"github.com/diamondburned/gotk4/pkg/gtk/v4"
+)
+
+func main() {
+	app := gtk.NewApplication("com.github.kristokarz.test.simple", gio.ApplicationFlagsNone)
+	app.ConnectActivate(func() { activate(app) })
+
+	if code := app.Run(os.Args); code > 0 {
+		os.Exit(code)
+	}
+}
+
+func activate(app *gtk.Application) {
+	window := gtk.NewApplicationWindow(app)
+	window.SetTitle("gotk4 Example")
+	window.SetChild(gtk.NewLabel("Hello from Kris!"))
+	window.SetDefaultSize(400, 300)
+	window.Show()
+}
