556c48dd42aab1a5beb9075398f0fb6a3321eb4d
Kris Tokarz <kj.tokarz@gmail.com>
2026-01-23 09:36:29 -0500

    Implement a basic UI and hook up the initial button for matrix creation

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"os"
+	"strconv"
 
 	"github.com/diamondburned/gotk4/pkg/gio/v2"
 	"github.com/diamondburned/gotk4/pkg/gtk/v4"
@@ -20,6 +21,24 @@ func activate(app *gtk.Application) {
 	builder := gtk.NewBuilderFromFile("./UI/main_ui.xml")
 
 	window := builder.GetObject("MainWindow").Cast().(*gtk.Window)
+
+	createMtxBtn := builder.GetObject("create_mtx_btn").Cast().(*gtk.Button)
+	createMtxBtn.Connect("clicked", func() {
+		rows_txt := builder.GetObject("mtx_rows").Cast().(*gtk.Text)
+		row, _ := strconv.Atoi(rows_txt.Text())
+
+		cols_txt := builder.GetObject("mtx_cols").Cast().(*gtk.Text)
+		cols, _ := strconv.Atoi(cols_txt.Text())
+
+		for i := 0; i < row; i++ {
+			for j := 0; j < cols; j++ {
+
+			}
+		}
+
+		//mtxGrid := builder.GetObject("grid_mtx").Cast().(*gtk.Grid)
+
+	})
 
 	app.AddWindow(window)
 
