eff72bff13d02c4583b1189e0e1f2bbb31b34a77
Kris Tokarz <kj.tokarz@gmail.com>
2026-01-31 13:15:27 -0500

    Implement build_matrix to create the UI

--- main.go
+++ main.go
@@ -23,24 +23,26 @@ func activate(app *gtk.Application) {
 	window := builder.GetObject("MainWindow").Cast().(*gtk.Window)
 
 	createMtxBtn := builder.GetObject("create_mtx_btn").Cast().(*gtk.Button)
-	createMtxBtn.Connect("clicked", func() {
-		rows_txt := builder.GetObject("mtx_rows").Cast().(*gtk.Text)
-		row, _ := strconv.Atoi(rows_txt.Text())
-
-		cols_txt := builder.GetObject("mtx_cols").Cast().(*gtk.Text)
-		cols, _ := strconv.Atoi(cols_txt.Text())
-
-		for i := 0; i < row; i++ {
-			for j := 0; j < cols; j++ {
-
-			}
-		}
-
-		//mtxGrid := builder.GetObject("grid_mtx").Cast().(*gtk.Grid)
-
-	})
+	createMtxBtn.Connect("clicked", func() { build_matrix(builder) })
 
 	app.AddWindow(window)
 
 	window.SetVisible(true)
+}
+
+func build_matrix(builder *gtk.Builder) {
+	rows_txt := builder.GetObject("mtx_rows").Cast().(*gtk.Text)
+	row, _ := strconv.Atoi(rows_txt.Text())
+
+	cols_txt := builder.GetObject("mtx_cols").Cast().(*gtk.Text)
+	cols, _ := strconv.Atoi(cols_txt.Text())
+
+	mtxGrid := builder.GetObject("grid_mtx").Cast().(*gtk.Grid)
+	for i := 0; i < row; i++ {
+		for j := 0; j < cols; j++ {
+			entry := gtk.NewEntry()
+			entry.SetPlaceholderText("0")
+			mtxGrid.Attach(entry, j, i, 1, 1)
+		}
+	}
 }
